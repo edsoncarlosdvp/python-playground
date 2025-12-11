c736d089cc1cd7262f4cee67d6a09dc8f04ff2f6
Edson Carlos <edsoncarlos@ymail.com>
2025-12-11 16:11:05 -0300

    FEAT: Model IA

---
+++ model_ia/model_ia.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+
+load_dotenv()
+
+api_key = os.getenv("GOOGLE_API_KEY")
+
+genai.configure(api_key=api_key)
+
+model = genai.GenerativeModel('gemini-2.5-flash')
+chat = model.start_chat(history=[])
+
+
+def model_ia(prompt=None):
+    if prompt:
+        response = chat.send_message(prompt)
+        return response.text
+
+    prompt = input("Digite para interagir com a IA: ")
+    while prompt.lower() != "sair":
+        response = chat.send_message(prompt)
+        print(f"IA: {response.text}\n")
+        prompt = input("Continue a sua interação com a IA (ou 'sair' para encerrar): ")
+    
+    print("Interação encerrada.")
