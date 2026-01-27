b38e9f70d88f7c4e775c3dcd75a635ed3f28af9c
Edson Carlos <edsoncarlos@ymail.com>
2026-01-27 17:17:03 -0300

    FEAT: Adding optional parameter delay

--- model_ia/model_ia_content.py
+++ model_ia/model_ia_content.py
@@ -1,5 +1,6 @@
-from dotenv import load_dotenv
 import os
+import time
+from dotenv import load_dotenv
 import google.generativeai as genai
 
 load_dotenv()
@@ -8,8 +9,11 @@ api_key = os.getenv("GOOGLE_API_KEY")
 
 genai.configure(api_key=api_key)
 
-def model_ia_content(prompt: str):
+def model_ia_content(prompt: str, delay: int = 0):
   model = genai.GenerativeModel("gemini-2.5-flash")
   response = model.generate_content(prompt)
+  
+  if delay > 0:
+      time.sleep(delay)
 
   return response.text
